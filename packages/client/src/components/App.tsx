e23c1aade9e3f89c34f80ff0158a3c6620b81a2d
Aleksey Inchakov <inchakov@gmail.com>
2023-03-19 16:11:03 -0400

    add bootstrap

---
+++ packages/client/src/components/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/esm/Form';
+
+function App() {
+  return (
+    <Container className="application-container">
+      <h1>Application</h1>
+      <Form>
+        <Form.Group className="application-input-group" controlId='firstName'>
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type='text' placeholder='John' />
+          <Form.Control.Feedback type='invalid'></Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className="application-input-group" controlId='lastName'>
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type='text' placeholder='Doe' />
+          <Form.Control.Feedback type='invalid'></Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className="application-input-group" controlId='dateOfBirth'>
+          <Form.Label>Date of Birth</Form.Label>
+          <Form.Control type='Date' />
+          <Form.Control.Feedback type='invalid'></Form.Control.Feedback>
+        </Form.Group>
+      </Form>
+    </Container>
+  );
+}
+
+export default App;
